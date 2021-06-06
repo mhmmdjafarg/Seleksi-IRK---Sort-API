c38efbf2d98e464b69c5cb51553552475ac3838c
mhmmdjafarg <mjafarashadiq712@gmail.com>
2021-06-06 21:03:30 +0700

    finish all

--- IOprocess.py
+++ IOprocess.py
@@ -1,5 +1,19 @@
 import csv
 
+def checkIfItemExist(jsonArray, item):
+    keys = list(jsonArray[0].keys())
+    count = 0
+    for i in jsonArray:
+        for key in keys:
+            if(i[key] != item[key]):
+                break
+            else:
+                count += 1
+        if(count == len(keys)):
+            return True
+        count = 0
+    return False
+
 def convertCSV (filepath):
     jsonArray = []
     #read csv file
@@ -11,4 +25,39 @@ def convertCSV (filepath):
         for row in csvReader: 
             #add this python dict to json array
             jsonArray.append(row)
+    
+    return removeInconsistentData(jsonArray)
+
+def removeInconsistentData(jsonArray):
+    keys = list(jsonArray[0].keys())
+    for key in keys:
+        length = len(jsonArray)
+        temp = []
+        othertemp = [] 
+        for i in range (length):
+            try:
+                data = float(jsonArray[i][key])
+                othertemp.append(jsonArray[i])
+            except ValueError:  
+                # print('masuk error di', jsonArray[i][key])
+                temp.append(jsonArray[i])
+        # remove num value
+        if len(temp) > len(othertemp):
+            jsonArray = temp.copy()
+        elif (len(temp) > 0):
+            jsonArray = othertemp.copy()
+        temp.clear()
+        othertemp.clear()
     return jsonArray
+
+
+# a = convertCSV('data.csv')
+# # c = a[0].copy()
+# # c['nomor'] = 100
+# # d = checkIfItemExist(a, c)
+# # print(a[0])
+# # print(c)
+# # print(d)
+# b = removeInconsistentData(a)
+# for i in b:
+#     print(i)
